60902aed95ef03da900df7bb30d80a536e1b4ea1
hush hush <60748867+Nahomna@users.noreply.github.com>
2025-04-14 08:48:56 +0300

    fix some error

---
+++ types/Course.ts
@@ -0,0 +1,13 @@
+// types/Course.ts
+
+export interface Course {
+    [x: string]: ReactNode
+    id: string
+    title: string
+    level: "undergraduate" | "graduate" | "doctoral"
+    format: "residential" | "online" | "hybrid"
+    duration: string
+    credits: number
+    description: string
+  }
+  
